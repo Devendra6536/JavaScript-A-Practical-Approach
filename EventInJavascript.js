88ef072ad37ed671282a5be3ea2477a7f2ee448a
Devendra kumar <devendrapal8682@gmail.com>
2022-11-27 19:16:16 +0530

    JavaScript Event is Done

---
+++ EventInJavascript.js
@@ -0,0 +1,17 @@
+function hel(){
+    document.write("Hello Devendra ");
+}
+function hell(){
+    document.write("Hello Devendra Kumar ");
+}
+function hello(){
+    document.write("Hello Devendra Kumar Pal");
+}
+function helll(){
+    document.write("Hello Devendra Kumar Pal Garhikhanpur");
+}
+
+function keyPressFucntion(){
+    var a  = document.getElementById("key").value;
+    document.write("You pressed " + a);
+}
