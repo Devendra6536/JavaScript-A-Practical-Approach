c60349ba79a2dd14a5e424d3abf216d758b16b28
Devendra kumar <devendrapal8682@gmail.com>
2022-11-27 18:30:33 +0530

    JavaScript Revision is ongoing

---
+++ Alert.js
@@ -0,0 +1,5 @@
+var a = confirm("Are you sure");
+alert(a);
+var b = prompt("Enter Your Name");
+var c = prompt("Enter Your Number");
+alert("Hello"+b+" And Your Number is :  "+ c );
---
+++ FuctionInJavaScript.js
@@ -0,0 +1,25 @@
+function Hello(){
+    document.write("Hello Devendra kumar ");
+}
+Hello();
+
+function Hel(){
+    var a = prompt("Enter Your Name");
+    var b = prompt("Enter Your DesigNation");
+
+    document.write("Hello" + a + " How Are you");
+}
+Hel();
+
+document.write("</br>")
+function Hell(a,b){
+    document.write("Hello" + a + " Your Aadhar Number is" + b);
+}
+Hell("Devendra Kumar","594304167744");
+
+
+function Power(a,b){
+    return a**b;
+}
+var c = Power(3,2);
+document.write("</br>" + c);
