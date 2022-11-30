43eaceafd028f5b545d391edde50ebd3bd0e09a6
Devendra kumar <devendrapal8682@gmail.com>
2022-11-30 18:46:28 +0530

    192.127.0.0.1

--- ObjectInJavaScript.js
+++ ObjectInJavaScript.js
@@ -24,3 +24,24 @@ document.write(user.fullname()+" ");
 document.write(user.living.city+" ");
 document.write(user.living.village+" ");
 document.write(user.living.pin+" ");
+
+
+
+var customer = new Object();
+customer.Name = "Devendra kumar";
+customer.age = 21;
+customer.village = "Ghaziabad";
+
+document.write(customer['Name']+"<br>");
+
+
+var student =[
+    {name:"Dk",rollNo:73},
+    {name:"Puneet",rollNo:25},
+    {name:"ak",rollNo:26},
+    {name:"Devansh",rollNo:72},
+];
+
+student.forEach(function(name){
+    document.write(name.name+" "+ name.rollNo+"<br>");
+})
