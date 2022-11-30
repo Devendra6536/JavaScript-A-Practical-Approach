d3eed33dd4b5fe74ea8145c16aeb64b4dc34aa71
Devendra kumar <devendrapal8682@gmail.com>
2022-11-30 20:20:11 +0530

    .

---
+++ DateInJavascript.js
@@ -0,0 +1,6 @@
+var now = new Date();
+document.write(now+"<br><br>");
+document.write(now.toDateString()+"<br><br>");
+document.write(now.getDay()+"<br><br>");
+document.write(now.getHours()+":" +now.getMinutes()+"<br><br>");
+document.write(now.getMilliseconds()+"<br><br>");
